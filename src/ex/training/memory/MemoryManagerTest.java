465a7f230cbaf5ca2f79a518d8eb59c1152ecf48
Roman <roman.oborin@orioninc.com>
2021-04-11 17:06:08 +0300

    Main logic

---
+++ src/ex/training/memory/MemoryManagerTest.java
@@ -0,0 +1,16 @@
+package ex.training.memory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemoryManagerTest {
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
